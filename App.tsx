6e50717bf6138a0a2f498f4dffadfc348b05ef12
rauloficial <eu.raulbraga@gmail.com>
2025-03-05 19:05:07 -0300

    Update App

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style='light' />
-      {fontsLoaded ? <Groups/> : <Loading/>}
+      {fontsLoaded ? <NewGroup/> : <Loading/>}
     </ThemeProvider>
   );
 }
